a252fa79d9e12b385deffb8fe9f540a2cf04d445
Izhan <babuizhan1234@gmail.com>
2022-06-04 13:56:43 +0530

    Added all front page components

--- App.js
+++ App.js
@@ -7,7 +7,21 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>This is the homepage</Text>
-      <Button title="Button" color="#ddd" />
+      <Text>
+        GPA Claculator, calculates your GPA for each semester as well as CGPA
+        for all the semesters combined. (Made for GPAs that are out of 10).
+      </Text>
+      <Text>No. of Semesters</Text>
+      <Button title="1 Semester" color="#ddd" />
+      <Button title="2 Semester" color="#ddd" />
+      <Button title="3 Semester" color="#ddd" />
+      <Button title="4 Semester" color="#ddd" />
+      <Button title="5 Semester" color="#ddd" />
+      <Button title="6 Semester" color="#ddd" />
+      <Button title="7 Semester" color="#ddd" />
+      <Button title="8 Semester" color="#ddd" />
+      <Button title="9 Semester" color="#ddd" />
+      <Button title="10 Semester" color="#ddd" />
     </View>
   );
 }

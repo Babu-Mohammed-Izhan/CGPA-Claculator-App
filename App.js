c15e982f7f502ea3c543ba5510ff5b0de6f017f6
Izhan <babuizhan1234@gmail.com>
2022-06-05 11:38:12 +0530

    removed unused imports

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import CalcButton from './components/CalcButton';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Homepage from './pages/Homepage';
@@ -9,18 +6,6 @@ import CalculatorPage from './pages/CalculatorPage';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const data = [
-    '1 Semester',
-    '2 Semester',
-    '3 Semester',
-    '4 Semester',
-    '5 Semester',
-    '6 Semester',
-    '7 Semester',
-    '8 Semester',
-    '9 Semester',
-    '10 Semester',
-  ];
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -34,33 +19,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginHorizontal: 'auto',
-    width: '80%',
-  },
-  description: {
-    fontSize: '1.5rem',
-  },
-  header: {
-    fontSize: '2rem',
-    marginBottom: '2rem',
-    fontWeight: 700,
-  },
-  box: {
-    width: '100%',
-    padding: '2rem',
-    borderColor: 'black',
-    borderWidth: '3px',
-    borderRadius: '10px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '2rem',
-  },
-});

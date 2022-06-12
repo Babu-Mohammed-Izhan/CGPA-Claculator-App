6800181c3e538439c9dc26e21bf841ea4dd8cf95
Izhan <babuizhan1234@gmail.com>
2022-06-12 23:49:52 +0530

    Fixed GPA Table

--- components/GPADataTable.js
+++ components/GPADataTable.js
@@ -5,10 +5,11 @@ import { View, Text } from 'react-native';
 const GpaDataTable = ({ gpaData, noOfSemesters }) => {
   const [Cgpa, setCgpa] = useState(0);
   useEffect(() => {
-    if (noOfSemesters === 1) {
-      setCgpa(gpaData[1].gpa);
+    console.log(noOfSemesters.length);
+    if (noOfSemesters.length === 1) {
+      setCgpa(gpaData['1 Semester'].gpa);
     }
-    if (noOfSemesters > 1) {
+    if (noOfSemesters.length > 1) {
       const numerator = Object.values(gpaData).reduce(
         (a, b) => a.numerator + b.numerator
       );
@@ -25,15 +26,17 @@ const GpaDataTable = ({ gpaData, noOfSemesters }) => {
       <View className="container gpa-data-table">
         <Text className="cgpa">CGPA : {Cgpa}</Text>
         <View>
-          {Object.keys(gpaData).map(([key, value]) => {
-            return (
-              <View key={key} className="gpa">
-                <Text>
-                  Semester {key} GPA : {gpaData[key].gpa}
-                </Text>
-              </View>
-            );
-          })}
+          {gpaData &&
+            Object.keys(gpaData).map(([key, value]) => {
+              console.log(key);
+              return (
+                <View key={key} className="gpa">
+                  <Text>
+                    Semester {key} GPA : {gpaData[`${key} Semester`].gpa}
+                  </Text>
+                </View>
+              );
+            })}
         </View>
       </View>
     </View>
